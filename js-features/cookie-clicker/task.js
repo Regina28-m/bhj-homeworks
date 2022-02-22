581ad45d967ab92b83a490bba1893a4033688ec7
Regina28-m <regina-railan2013@yandex.ru>
2022-02-22 22:54:04 +0300

    commit

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,21 +1,13 @@
-'use strict';
-(() => {
-const click = document.getElementById('clicker__counter');
-let counterClicker = parseInt(click.textContent);
-const cookies = document.getElementById('cookie');
-const speed = document.getElementById('clicker__speeder');
-const date = Date.now();
-let dateNow;
-console.log(date);
-cookies.onclick = () => {
-	counterClicker++;
-	dateNow = Date.now();
-	click.textContent = counterClicker;
-	speed.textContent = ((counterClicker) / ((dateNow - date) * 0.001)).toFixed(2);
-	if (cookies.width === 200) {
-		cookies.width = 300;
-	} else {
-		cookies.width = 200;
-	}
+const cookie = document.getElementById("cookie")
+let timeLastClick = new Date();
+
+countAndChangeSize = () => {
+    const clicker__counter = document.getElementById("clicker__counter");
+    clicker__counter.textContent++; 
+    if (cookie.width == 200) cookie.width = 250;
+    else cookie.width = 200;
+    const speedClick = document.getElementById("speedClick");
+    speedClick.textContent = (1 / ((new Date() - timeLastClick) / 1000 )).toFixed(2);
+    timeLastClick = new Date();
 }
-}) ()
+cookie.onclick = countAndChangeSize;
